17d80462f13d1b8d4b48fcecdab7b6efac0dc48f
Angel Miranda <juan.miranda@gmail.com>
2023-05-11 16:30:57 -0400

    Seleccionando Multiples Actores

--- src/peliculas/FormularioPeliculas.tsx
+++ src/peliculas/FormularioPeliculas.tsx
@@ -12,6 +12,7 @@ import { generoDTO } from "../generos/generos.model";
 import { useState } from "react";
 import { cineDTO } from "../cines/cines.model";
 import TypeAheadActores from "../actores/TypeAheadActores";
+import { actorPeliculaDTO } from "../actores/actores.model";
 
 export default function FormularioPeliculas(props: formularioPeliculasProps){
     const [generosSeleccionados, setGenerosSeleccionados] = useState(mapear(props.generosSeleccionados));
@@ -19,6 +20,8 @@ export default function FormularioPeliculas(props: formularioPeliculasProps){
 
     const [cinesSeleccionados, setCinesSeleccionados] = useState(mapear(props.cinesSeleccionados));
     const [cinesNoSeleccionados, setCinesNoSeleccionados] = useState(mapear(props.cinesNoSeleccionados));
+
+    const [actoresSeleccionados, setActoresSeleccionados] = useState<actorPeliculaDTO[]>([])
 
     function mapear(arreglo: {id: number, nombre: string}[]): selectorMultipleModel[]{
         return arreglo.map(valor => {
@@ -63,7 +66,27 @@ export default function FormularioPeliculas(props: formularioPeliculasProps){
 
                     <div className="form-group">
                         <TypeAheadActores 
-                            actores={[]}
+                            onAdd={actores => {
+                                setActoresSeleccionados(actores);
+                            }}
+                            onRemove={actor => {
+                                const actores = actoresSeleccionados.filter(x => x !== actor);
+                                setActoresSeleccionados(actores);
+                            }}
+                            actores={actoresSeleccionados}
+                            listadoUI={(actor: actorPeliculaDTO) => 
+                            <>
+                                {actor.nombre} / <input placeholder="Personaje" 
+                                type="text" 
+                                value={actor.personaje}
+                                onChange={e => {
+                                    const indice = actoresSeleccionados.findIndex(x => x.id === actor.id);
+                                    const actores = [...actoresSeleccionados];
+                                    actores[indice].personaje = e.currentTarget.value;
+                                    setActoresSeleccionados(actores);
+                                }}
+                                />
+                            </>}
                         />
 
                     </div>
